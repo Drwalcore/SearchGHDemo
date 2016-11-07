92a618e054f6a7f9af5432e80052c231bbc0d19b
Michał Czerniakowski <mpczerniakowski@gmail.com>
2016-11-07 06:03:43 +0100

    Added Cell Custom class, set up main ViewController to conform TableView protocol.

---
+++ SearchGitHubAPI/CustomGITCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomGITCell.swift
+//  SearchGitHubAPI
+//
+//  Created by Michał Czerniakowski on 07.11.2016.
+//  Copyright © 2016 Michał Czerniakowski. All rights reserved.
+//
+
+import Foundation
+
+class CustomGITCell: UITableViewCell{
+
+
+}
--- SearchGitHubAPI/ViewController.swift
+++ SearchGitHubAPI/ViewController.swift
@@ -8,36 +8,121 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var searchBar: UISearchBar!
+    var searchPreRequisities = GitHubAPIConditions()
+    var GitRepositories: [GitHubAPI]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        searchBarSetUp()
+        tableViewPreSetup()
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func tableViewPreSetup() {
+    
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 100
+    }
+    
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var reusableCell = tableView.dequeueReusableCell(withIdentifier: "CustomGITCell", for: indexPath) as! CustomGITCell
+        
+        
+        return reusableCell
+    
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let GitRepositories = GitRepositories {
+            
+            return GitRepositories.count
+        } else {
+            
+            return 0
+        }
 
+    }
+    
+        
+    func searchBarSetUp() {
+    
+        searchBar = UISearchBar()
+        searchBar.delegate = self
+        
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+    }
+    
+    private func searchInit(){
+        
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        
+        GitHubAPI.startRepoFetch(settings: searchPreRequisities, successCallback: { (repositories) -> Void in
+            self.GitRepositories = repositories
+            for repository in repositories {
+                
+                print("[Name: \(repository.name!)]" +
+                    "\n\t[Description: \(repository.description!)]" +
+                    "\n\t[Stars: \(repository.stars!)]" +
+                    "\n\t[Owner: \(repository.ownerLogin!)]" +
+                    "\n\t[Avatar: \(repository.ownerAvatarURL!)]")
+                //var singleRepo = ["name" : repo.name, "description" : repo.description]  as? NSDictionary
+                //self.repositories = singleRepo as? [NSDictionary]
+            }
+            
+            MBProgressHUD.hide(for: self.view, animated: true)
+            self.tableView.reloadData()
+        }, error: { (error) -> Void in
+            
+            print(error!)
+            
+        })
+    }
 }
+
 
 extension ViewController: UISearchBarDelegate {
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        
         searchBar.setShowsCancelButton(true, animated: true)
         return true;
     }
     
     
     func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        
         searchBar.setShowsCancelButton(false, animated: true)
         return true;
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        
         searchBar.text = ""
         searchBar.resignFirstResponder()
     }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchPreRequisities.searchString = searchBar.text
+        searchBar.resignFirstResponder()
+//        doSearch()
+    }
+
     
 }
